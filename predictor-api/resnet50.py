b4896f3adc20924a42954ff7a4611115e4b8f9da
owenburton <owenburton@users.noreply.github.com>
2019-11-19 10:53:12 -0800

    updated predictor-api.py

--- predictor-api/resnet50.py
+++ predictor-api/resnet50.py
@@ -1,13 +1,24 @@
 import numpy as np
+import requests
+from PIL import Image
+from io import BytesIO
 
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
 
-url = 'https://en.wikipedia.org/wiki/Gandalf#/media/File:Gandalf600ppx.jpg'
 
-def process_img_path(img_path):
-  ''' Processes image url and compresses it to 224 x 224 pixels'''
-  return image.load_img(img_path, target_size=(224, 224))
+def process_img_path(url):
+    ''' Processes image url and compresses it to 224 x 224 pixels'''
+    if url.startswith('http://') or url.startswith('https://') or url.startswith('ftp://'):
+        response = requests.get(url)
+        img = Image.open(BytesIO(response.content))
+        img = img.resize((224, 224))
+    else:
+        if not os.path.exists(url):
+            raise Exception('Input image file does not exist')
+        img = image.load_img(url, target_size=(224, 224))
+    return img
+
 
 def resnet_model(img):
     ''' Processes image into an array of vectors
@@ -19,7 +30,9 @@ def resnet_model(img):
     model = ResNet50(weights='imagenet')
     features = model.predict(x)
     results = decode_predictions(features, top=3)[0]
-    preds = {tup[1]:tup[2] for tup in results}
+    preds = {tup[1]: tup[2] for tup in results}
     return preds
 
-print(resnet_model(process_img_path(url)))
+# url = 'https://upload.wikimedia.org/wikipedia/en/e/e9/Gandalf600ppx.jpg'
+
+# print(resnet_model(process_img_path(url)))
