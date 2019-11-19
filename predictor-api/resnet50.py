bd6f9b4e6c4c1dbf8369c68434f4002ab97f8ce2
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-11-19 08:35:10 -0800

    continued work on url processing, pep8 conformity

--- predictor-api/resnet50.py
+++ predictor-api/resnet50.py
@@ -1,24 +1,24 @@
 import numpy as np
 
-from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
+from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input
+from tensorflow.keras.applications.resnet50 import decode_predictions
 from tensorflow.keras.preprocessing import image
 
-from PIL import Image
-import requests
-from io import BytesIO
-
+from imageio import imread
 
 url = 'https://en.wikipedia.org/wiki/Gandalf#/media/File:Gandalf600ppx.jpg'
 
+
 def process_img_path_url(img_path):
     ''' Process image url and compresses it to 224 x 224'''
-    response = requests.get(img_path)
-    img = Image.open(BytesIO(response.content))
+    img = imread(img_path)
     return img
 
+
 def process_img_path_local(img_path):
-  ''' Processes local image path and compresses it to 224 x 224 pixels'''
-  return image.load_img(img_path, target_size=(224, 224))
+    ''' Processes local image path and compresses it to 224 x 224 pixels'''
+    return image.load_img(img_path, target_size=(224, 224))
+
 
 def resnet_model(img):
     ''' Processes image into an array of vectors
@@ -30,7 +30,8 @@ def resnet_model(img):
     model = ResNet50(weights='imagenet')
     features = model.predict(x)
     results = decode_predictions(features, top=3)[0]
-    preds = {tup[1]:tup[2] for tup in results}
+    preds = {tup[1]: tup[2] for tup in results}
     return preds
 
-print(resnet_model(process_img_path(url)))
+
+print(resnet_model(process_img_path_url(url)))
