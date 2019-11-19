56ec2d6c1aac7f8c3b5e9f0beef0500f35b1ed35
KyleHaggin <KyleHaggin@users.noreply.github.com>
2019-11-19 08:28:40 -0800

    Updated resnet50 functionality to work with online urls

--- predictor-api/resnet50.py
+++ predictor-api/resnet50.py
@@ -3,10 +3,21 @@ import numpy as np
 from tensorflow.keras.applications.resnet50 import ResNet50, preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
 
+from PIL import Image
+import requests
+from io import BytesIO
+
+
 url = 'https://en.wikipedia.org/wiki/Gandalf#/media/File:Gandalf600ppx.jpg'
 
-def process_img_path(img_path):
-  ''' Processes image url and compresses it to 224 x 224 pixels'''
+def process_img_path_url(img_path):
+    ''' Process image url and compresses it to 224 x 224'''
+    response = requests.get(img_path)
+    img = Image.open(BytesIO(response.content))
+    return img
+
+def process_img_path_local(img_path):
+  ''' Processes local image path and compresses it to 224 x 224 pixels'''
   return image.load_img(img_path, target_size=(224, 224))
 
 def resnet_model(img):
